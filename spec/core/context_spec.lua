1b6a4b512ea4a16d00f06aa4b09923efbdef4d1b
Afonso Bordado <afonsobordado@az8.co>
2017-07-27 20:56:24 +0100

    Stuff

--- spec/core/context_spec.lua
+++ spec/core/context_spec.lua
@@ -1,5 +1,9 @@
+local llvm = require "llvm"
+
 describe("Context module", function ()
 	it("should cleanly allocate and deallocate global context", function ()
+    local context = llvm.Context.get_global_context()
+    
 		assert.truthy("ay")
 	end)
 end)
