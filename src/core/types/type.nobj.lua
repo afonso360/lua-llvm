3b854e86c0bc61f1cba805b18855ec194ba51458
Afonso Bordado <afonsobordado@az8.co>
2017-07-31 21:19:20 +0100

    Remove unintentional duplicate variable

--- src/core/types/type.nobj.lua
+++ src/core/types/type.nobj.lua
@@ -25,72 +25,66 @@ object "Type" {
   constructor "int_1" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMInt1Type();
+         ${this} = LLVMInt1Type();
       } else {
-         ${type} = LLVMInt1TypeInContext(${ctx});
+         ${this} = LLVMInt1TypeInContext(${ctx});
       }
     ]],
   },
   constructor "int_8" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMInt8Type();
+         ${this} = LLVMInt8Type();
       } else {
-         ${type} = LLVMInt8TypeInContext(${ctx});
+         ${this} = LLVMInt8TypeInContext(${ctx});
       }
     ]],
   },
   constructor "int_16" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMInt16Type();
+         ${this} = LLVMInt16Type();
       } else {
-         ${type} = LLVMInt16TypeInContext(${ctx});
+         ${this} = LLVMInt16TypeInContext(${ctx});
       }
     ]],
   },
   constructor "int_32" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMInt32Type();
+         ${this} = LLVMInt32Type();
       } else {
-         ${type} = LLVMInt32TypeInContext(${ctx});
+         ${this} = LLVMInt32TypeInContext(${ctx});
       }
     ]],
   },
   constructor "int_64" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMInt64Type();
+         ${this} = LLVMInt64Type();
       } else {
-         ${type} = LLVMInt64TypeInContext(${ctx});
+         ${this} = LLVMInt64TypeInContext(${ctx});
       }
     ]],
   },
   constructor "int_128" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMInt128Type();
+         ${this} = LLVMInt128Type();
       } else {
-         ${type} = LLVMInt128TypeInContext(${ctx});
+         ${this} = LLVMInt128TypeInContext(${ctx});
       }
     ]],
   },
@@ -99,12 +93,11 @@ object "Type" {
     doc [[ Bits: Size of the integer ]],
     var_in { "unsigned", "bits" },
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMIntType(${bits});
+         ${this} = LLVMIntType(${bits});
       } else {
-         ${type} = LLVMIntTypeInContext(${ctx}, ${bits});
+         ${this} = LLVMIntTypeInContext(${ctx}, ${bits});
       }
     ]],
   },
@@ -113,76 +106,70 @@ object "Type" {
     c_method_call "unsigned" "LLVMGetIntTypeWidth" {}
   },
 
-  --                                          Floating Point types
+  --                                          Floating Pointthis 
   constructor "half" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMHalfType();
+         ${this} = LLVMHalfType();
       } else {
-         ${type} = LLVMHalfTypeInContext(${ctx});
+         ${this} = LLVMHalfTypeInContext(${ctx});
       }
     ]],
   },
   constructor "float" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMFloatType();
+         ${this} = LLVMFloatType();
       } else {
-         ${type} = LLVMFloatTypeInContext(${ctx});
+         ${this} = LLVMFloatTypeInContext(${ctx});
       }
     ]],
   },
   constructor "double" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMDoubleType();
+         ${this} = LLVMDoubleType();
       } else {
-         ${type} = LLVMDoubleTypeInContext(${ctx});
+         ${this} = LLVMDoubleTypeInContext(${ctx});
       }
     ]],
   },
   constructor "x86fp80" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMX86FP80Type();
+         ${this} = LLVMX86FP80Type();
       } else {
-         ${type} = LLVMX86FP80TypeInContext(${ctx});
+         ${this} = LLVMX86FP80TypeInContext(${ctx});
       }
     ]],
   },
   constructor "fp128" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMFP128Type();
+         ${this} = LLVMFP128Type();
       } else {
-         ${type} = LLVMFP128TypeInContext(${ctx});
+         ${this} = LLVMFP128TypeInContext(${ctx});
       }
     ]],
   },
   constructor "ppcfp128" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMPPCFP128Type();
+         ${this} = LLVMPPCFP128Type();
       } else {
-         ${type} = LLVMPPCFP128TypeInContext(${ctx});
+         ${this} = LLVMPPCFP128TypeInContext(${ctx});
       }
     ]],
   },
@@ -191,36 +178,33 @@ object "Type" {
   constructor "void" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMVoidType();
+         ${this} = LLVMVoidType();
       } else {
-         ${type} = LLVMVoidTypeInContext(${ctx});
+         ${this} = LLVMVoidTypeInContext(${ctx});
       }
     ]],
   },
   constructor "label" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMLabelType();
+         ${this} = LLVMLabelType();
       } else {
-         ${type} = LLVMLabelTypeInContext(${ctx});
+         ${this} = LLVMLabelTypeInContext(${ctx});
       }
     ]],
   },
   constructor "x86mmx" {
     doc [[ Context is optional ]],
     var_in { "Context *", "ctx?" },
-    var_out { "Type *", "type" },
     c_source [[
       if (${ctx} == NULL) {
-         ${type} = LLVMX86MMXType();
+         ${this} = LLVMX86MMXType();
       } else {
-         ${type} = LLVMX86MMXTypeInContext(${ctx});
+         ${this} = LLVMX86MMXTypeInContext(${ctx});
       }
     ]],
   },
