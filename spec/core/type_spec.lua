5859f36ea11a529360f0e7886149270c4ff86d55
Afonso Bordado <afonsobordado@az8.co>
2017-08-01 19:11:25 +0100

    Add License headers

--- spec/core/type_spec.lua
+++ spec/core/type_spec.lua
@@ -1,3 +1,9 @@
+-- Copyright (C) 2017 Afonso Bordado
+--
+-- Licensed under the MIT license <http://opensource.org/licenses/MIT>,
+-- This file may not be copied, modified, or distributed except according
+-- to the terms of that license.
+
 local llvm = require "llvm"
 
 describe("Type module", function ()
